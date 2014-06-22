07fef1d13e70e552977a67ac3a540f0f31748d53
Patrick Altman <paltman@gmail.com>
2014-06-21 23:33:03 -0500

    Require login and restrict objects to your own

--- pto/views.py
+++ pto/views.py
@@ -2,27 +2,32 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.views.generic import CreateView, ListView, UpdateView
 
+from account.mixins import LoginRequiredMixin
+
 from .forms import ContactForm, StudentForm
 from .models import Contact, Student
 
 
-class ContactsView(ListView):
+class ContactsView(LoginRequiredMixin, ListView):
     model = Contact
 
     def get_queryset(self):
         return self.request.user.family.contacts.all().order_by("pk")
 
 
-class StudentsView(ListView):
+class StudentsView(LoginRequiredMixin, ListView):
     model = Student
 
     def get_queryset(self):
         return self.request.user.family.students.all().order_by("pk")
 
 
-class ContactCreateView(CreateView):
+class ContactCreateView(LoginRequiredMixin, CreateView):
     form_class = ContactForm
     model = Contact
+
+    def get_queryset(self):
+        return self.request.user.family.contacts.all().order_by("pk")
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -31,17 +36,23 @@ class ContactCreateView(CreateView):
         return redirect("contacts")
 
 
-class ContactUpdateView(UpdateView):
+class ContactUpdateView(LoginRequiredMixin, UpdateView):
     form_class = ContactForm
     model = Contact
+
+    def get_queryset(self):
+        return self.request.user.family.contacts.all().order_by("pk")
 
     def get_success_url(self):
         return reverse("contacts")
 
 
-class StudentCreateView(CreateView):
+class StudentCreateView(LoginRequiredMixin, CreateView):
     form_class = StudentForm
     model = Student
+
+    def get_queryset(self):
+        return self.request.user.family.students.all().order_by("pk")
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -50,9 +61,12 @@ class StudentCreateView(CreateView):
         return redirect("students")
 
 
-class StudentUpdateView(UpdateView):
+class StudentUpdateView(LoginRequiredMixin, UpdateView):
     form_class = StudentForm
     model = Student
+
+    def get_queryset(self):
+        return self.request.user.family.students.all().order_by("pk")
 
     def get_success_url(self):
         return reverse("students")
